d457389a4946049ac87fa411e57e3190d114b325
Arbab Ali <arbabali1441@gmail.com>
2020-09-17 00:08:11 +0500

    completed like feature 5.4

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,6 @@ Rails.application.routes.draw do
 
   resources :posts, only: [:index, :show, :create, :destroy] do
     resources :photo, only: [:create]
+    resources :likes, only: [:create, :destroy], shallow: true
   end
 end
